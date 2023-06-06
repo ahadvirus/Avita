0db2d5c8c60798d4d278fb262da628404095b78a
ahadvirus <ahadvirus@yahoo.com>
2023-06-06 17:08:35 +0330

    Migration helper class

---
+++ Apps/Avita.Identity/Data/Contracts/IMigratorService.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+
+namespace Avita.Identity.Data.Contracts;
+
+public interface IMigratorService
+{
+    /// <summary>
+    /// Checking the database exists in sql server
+    /// </summary>
+    /// <returns></returns>
+    Task EnsureDatabase();
+
+    /// <summary>
+    /// Applied the migration not applied yet on sql server
+    /// </summary>
+    /// <returns></returns>
+    Task MigrateUp();
+
+    /// <summary>
+    /// Rollback last migration applied in database
+    /// </summary>
+    /// <returns></returns>
+    Task MigrateDown();
+}
---
+++ Apps/Avita.Identity/Infrastructure/Extensions/ApplyMigrationExtension.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Avita.Identity.Infrastructure.Extensions;
+
+public static class ApplyMigrationExtension
+{
+
+    public static async void ApplyMigration(this WebApplication app)
+    {
+        using (IServiceScope scope = app.Services.CreateScope())
+        {
+            Data.Contracts.IMigratorService?
+                service = scope.ServiceProvider.GetService<Data.Contracts.IMigratorService>();
+
+            if (service != null)
+            {
+                await service.MigrateUp();
+            }
+        }
+    }
+}
