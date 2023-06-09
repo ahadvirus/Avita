c211fee6665e52653ec31ef98d854007186d6cf7
ahadvirus <ahadvirus@yahoo.com>
2023-06-09 16:42:01 +0330

    Create new entity and dto for it

---
+++ Libraries/Avita.Models/DataTransfers/ProductDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Avita.Models.DataTransfers;
+
+public record ProductDto
+{
+    public int Id { get; init; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; init; } = string.Empty;
+    public long Price { get; set; }
+    public string Image { get; set; } = string.Empty;
+    public string Type { get; set; } = string.Empty;
+    public string Brand { get; set; } = string.Empty;
+    public int Quantity { get; set; }
+}
---
+++ Libraries/Avita.Models/Entities/Base/Post.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Avita.Models.Entities.Base;
+
+public class Post
+{
+    public virtual Guid Id { get; set; }
+    
+    public virtual string Type { get; set; } = string.Empty;
+    
+    public virtual string Key { get; set; } = string.Empty;
+    
+    public virtual string Value { get; set; } = string.Empty;
+    
+    public virtual Guid Group { get; set; }
+}
