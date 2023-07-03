7b7b58df4f912d16133e94260f8b9ff506dfbaa7
AhadVirus <ahadvirus@yahoo.com>
2023-07-03 12:31:43 +0330

    Trying to create login page for system

---
+++ Apps/Modules/Avita.Identity/Controllers/LoginController.cs
@@ -0,0 +1,55 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Avita.Identity.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using NHibernate;
+using NHibernate.Linq;
+
+namespace Avita.Identity.Controllers;
+
+public class LoginController : Controller
+{
+    [HttpGet(template: nameof(Models.Configurations.Route.Auth) + "/[controller]/[action]")]
+    public Task<IActionResult> Index([FromQuery] string returnUrl)
+    {
+        return Task.FromResult<IActionResult>(View(model: new LoginVm() { ReturnUrl = returnUrl }));
+    }
+
+    [ValidateAntiForgeryToken]
+    [HttpPost(template: nameof(Models.Configurations.Route.Auth) + "/[controller]/[action]")]
+    public async Task<IActionResult> Index(
+        [Bind(include: new string[]
+        {
+            nameof(LoginVm.Username),
+            nameof(LoginVm.Password),
+            nameof(LoginVm.RememberMe),
+            nameof(LoginVm.ReturnUrl)
+        })]
+        LoginVm entry,
+        [FromServices] ISessionFactory sessionFactory,
+        CancellationToken cancellationToken)
+    {
+        
+        IActionResult result = View(model: entry);
+        
+        if (ModelState.IsValid)
+        {
+            using (ISession session = sessionFactory.OpenSession())
+            {
+                IQueryable<Avita.Models.Entities.Identity.User> users =
+                    session.Query<Avita.Models.Entities.Identity.User>();
+
+                if (await users
+                        .Where(predicate: user => user.Username == entry.Username && user.Password == entry.Password)
+                        .Select(selector: user => user.Id)
+                        .AnyAsync(cancellationToken: cancellationToken))
+                {
+                    //
+                }
+            }
+        }
+
+        return result;
+    }
+}
---
+++ Apps/Modules/Avita.Identity/Models/ViewModels/LoginVm.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Avita.Identity.Models.ViewModels;
+
+public class LoginVm
+{
+    public LoginVm()
+    {
+        ReturnUrl = string.Empty;
+        Username = string.Empty;
+        Password = string.Empty;
+    }
+    public string ReturnUrl { get; set; }
+    
+    [Required]
+    public string Username { get; set; }
+    
+    [Required]
+    public string Password { get; set; }
+    
+    public bool RememberMe { get; set; }
+}
