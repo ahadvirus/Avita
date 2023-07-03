f8c02b89c8c6eba1c190c293a5a472c569cda110
AhadVirus <ahadvirus@yahoo.com>
2023-07-03 12:28:47 +0330

    Add new feature to replace additional part

---
+++ Libraries/Avita.Infrastructure/Extensions/RemoveControllerExtension.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Avita.Infrastructure.Extensions;
+
+public static class RemoveControllerExtension
+{
+    public static string RemoveController(this string name)
+    {
+        return name.Replace(oldValue: nameof(Controller), newValue: string.Empty);
+    }
+}
