fb500c7c3a81973b4127709799192fe8c4f44529
AhadVirus <ahadvirus@yahoo.com>
2023-07-04 13:14:16 +0330

    Create Role entity

---
+++ Libraries/Avita.Models/Entities/Identity/Role.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Avita.Models.Entities.Identity;
+
+/// <summary>
+/// Using class for authorization system
+/// </summary>
+public class Role
+{
+    /// <summary>
+    /// Primary key in the table
+    /// </summary>
+    public virtual Guid Id { get; set; }
+    
+    /// <summary>
+    /// Store name of the role
+    /// </summary>
+    public virtual string Name { get; set; } = string.Empty;
+}
